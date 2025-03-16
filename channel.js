9a4d779aa909d25d9ed29a6138b27ee37a46f18b
Jayce <kendump666@gmail.com>
2025-03-16 12:52:52 +0800

    Create channel.js

---
+++ channel.js
@@ -0,0 +1,60 @@
+
+// Define all TV channels with their details
+const channels = {
+  trt1: {
+    name: "TRT 1",
+    number: 1,
+    url: "https://tv-trt1.medya.trt.com.tr/master_720.m3u8",
+    key: null
+  },
+  atv: {
+    name: "ATV",
+    number: 2,
+    url: "https://trkvz-live.daioncdn.net/atv/atv.m3u8",
+    key: null
+  },
+  showtv: {
+    name: "Show TV",
+    number: 3,
+    url: "https://showtvdokuzcom.daioncdn.net/showtv/showtv.m3u8?app=7ddc255a-ef47-4e81-ab14-c0e5c2a7fff9",
+    key: null
+  },
+  kanal7: {
+    name: "Kanal 7",
+    number: 4,
+    url: "https://kanal7-live.daioncdn.net/kanal7/kanal7.m3u8",
+    key: null
+  },
+  startv: {
+    name: "Star TV",
+    number: 5,
+    url: "https://startv.blutv.com/blutv_startv_live/live.m3u8",
+    key: null
+  },
+  fox: {
+    name: "FOX",
+    number: 6,
+    url: "https://foxtv.blutv.com/blutv_foxtv_live/live.m3u8",
+    key: null
+  },
+  tv8: {
+    name: "TV8",
+    number: 7,
+    url: "https://tv8-live.daioncdn.net/tv8/tv8.m3u8",
+    key: null
+  },
+  tlc: {
+    name: "TLC",
+    number: 8,
+    url: "https://tlc.blutv.com/blutv_tlc_live/live.m3u8",
+    key: null
+  },
+  dmax: {
+    name: "DMAX",
+    number: 9,
+    url: "https://dmax.blutv.com/blutv_dmax_live/live.m3u8",
+    key: null
+  }
+};
+
+export default channels;
